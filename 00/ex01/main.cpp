80eb482694a0a503551ff221a4e1121c42e650f3
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-12-28 18:28:21 +0100

    gitignore

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -8,7 +8,7 @@ int	main()
 	PhoneBook	phoneBook;
 	std::string	input;
 
-	std::cout << "Welcome the the Foking awsome Phone Book form 80's" << std::endl;
+	std::cout << "Welcome the the awsome Phone Book form 80's" << std::endl;
 	std::cout << "The program only accepts ADD, SEARCH and EXIT." << std::endl;
 	std::cout << "ADD: save a new contact" << std::endl;
 	std::cout << "SEARCH: display specific contact" << std::endl;
