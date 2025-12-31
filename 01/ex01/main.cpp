77338e825e252b381de8f0a73c6f3d8391fdf251
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2025-12-31 14:25:38 +0100

    ex01 done

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -3,9 +3,9 @@
 #include "Zombie.hpp"
 
 int	main() {
-
-	Zombie *hored = zombieHorde(5, "!!ZombiezzzZ!!");
-	for (int i = 0; i < 5; i++)
+	int		N = 5;
+	Zombie *hored = zombieHorde(N, "!!ZombiezzzZ!!");
+	for (int i = 0; i < N; i++)
 		hored[i].announce();
 	delete[] hored;
 	return 0;
