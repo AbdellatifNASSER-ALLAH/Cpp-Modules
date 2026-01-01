9624ad2c178c1c8330b9962720698158e12de07d
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-01-01 13:38:45 +0100

    Correct

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -12,7 +12,7 @@ int	main()
 	std::cout << "Address held by stringPTR       : " << stringPTR << std::endl;
 	std::cout << "Address held by stringREF       : " << &stringREF << std::endl;
 	std::cout << "Value of the string variable    : " << str << std::endl;
-	std::cout << "Valude pointed to by stringPTR  : " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringPTR  : " << *stringPTR << std::endl;
 	std::cout << "Value pointed to by stringREF   : " << stringREF << std::endl;
 	return (0);
 }
