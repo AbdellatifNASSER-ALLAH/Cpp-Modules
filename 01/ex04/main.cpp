25d3340171d6311b9ed5ec81e82333120c322007
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-01-01 14:11:06 +0100

    better impelementatoin

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -1,13 +1,16 @@
 
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 int	main(int ac, char **av)
 {
 	if (ac != 4) 
 		return (std::cout << "Usage: " << av[0] << " <filename> <replace> <with>" << std::endl, 1);
+
+	if (!av[2][0])
+		return (std::cout << "Error: <replace> strings must not be empty." << std::endl, 1);
 
 	std::ifstream	inputFile(av[1]);
 	std::string	outputFileName = std::string(av[1]) + ".replace";
@@ -16,21 +19,22 @@ int	main(int ac, char **av)
 	if (inputFile.is_open() == false || outputFile.is_open() == false)
 		return (std::cout << "Error: could not open file " << av[1] << std::endl, 1);
 
-	std::string	line;
-	std::string 	replace = av[2];
-	std::string 	with = av[3];
+	std::stringstream buffer;
+	buffer << inputFile.rdbuf();
+	std::string content = buffer.str();
 	
-	while(std::getline(inputFile, line))
+	std::string replace = av[2];
+	std::string with = av[3];
+	
+	size_t pos = 0;
+	while ((pos = content.find(replace, pos)) != std::string::npos)
 	{
-		size_t	pos = 0;
-		while ((pos = line.find(replace, pos)) != std::string::npos)
-		{
-			line.erase(pos, replace.length());
-			line.insert(pos, with);
-			pos += with.length();
-		}
-		outputFile << line << std::endl;
+		content.erase(pos, replace.length());
+		content.insert(pos, with);
+		pos += with.length();
 	}
+	
+	outputFile << content;
 	inputFile.close();
 	outputFile.close();
 	return (0);
