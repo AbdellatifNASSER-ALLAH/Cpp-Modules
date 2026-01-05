c1da5327df763757c6c7456afced0c53c9340efa
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-01-05 14:24:54 +0100

    ex01_done

--- 02/ex01/Fixed.hpp
+++ 02/ex01/Fixed.hpp
@@ -6,21 +6,21 @@
 #include <iostream>
 
 class Fixed {
-private:
-    int                 value;
-    static const int    fractionalBits = 8;
-public:
-    Fixed();
-    Fixed(const int ivalue);
-    Fixed(const float fvalue);
-    Fixed(const Fixed& other);
-    Fixed& operator=(const Fixed& other);
-    ~Fixed();
+	private:
+		int                 value;
+		static const int    fractionalBits = 8;
+	public:
+		Fixed();
+		Fixed(const int ivalue);
+		Fixed(const float fvalue);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
 
-    int		getRawBits() const;
-    void	setRawBits(int const raw);
-    float	toFloat() const;
-    int		toInt() const;
+		int		getRawBits() const;
+		void	setRawBits(int const raw);
+		float	toFloat() const;
+		int		toInt() const;
 };
 
 std::ostream&	operator<<(std::ostream& os, const Fixed& fixed);
