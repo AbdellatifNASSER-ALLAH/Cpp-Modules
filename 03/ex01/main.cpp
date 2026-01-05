c9d7ef7e7a2a1c3e1ea9b0ba06f04011acdb4ed4
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-01-05 22:48:56 +0100

    ex01_init

--- 03/ex01/main.cpp
+++ 03/ex01/main.cpp
@@ -1,20 +1,19 @@
 
 
-#include <iostream>
 #include "ClapTrap.hpp"
-	
+
 
 int main() {
-    ClapTrap clap1("Clappy");
-    ClapTrap clap2("Trapster");
+	ClapTrap clap1("Clappy");
+	ClapTrap clap2("Trapster");
 
-    clap1.attack("Trapster");
-    clap2.takeDamage(5);
-    clap2.beRepaired(3);
+	clap1.attack("Trapster");
+	clap2.takeDamage(5);
+	clap2.beRepaired(3);
 
-    clap1.attack("Trapster");
-    clap2.takeDamage(5);
-    clap2.beRepaired(10);
+	clap1.attack("Trapster");
+	clap2.takeDamage(5);
+	clap2.beRepaired(10);
 
-    return 0;
+	return 0;
 }
