63a6aa0e9783040aa434601fb97944fb173f85ba
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-01-05 14:07:56 +0100

    ex01_dont

--- 02/ex01/Fixed.cpp
+++ 02/ex01/Fixed.cpp
@@ -1,9 +1,19 @@
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed() : value(0) {
 	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int ivalue) {
+	std::cout << "Int constructor called" << std::endl;
+	this->value = ivalue << fractionalBits;
+}
+Fixed::Fixed(const float fvalue) {
+	std::cout << "Float constructor called" << std::endl;
+	this->value = roundf(fvalue * (1 << fractionalBits));
 }
 
 Fixed::Fixed(const Fixed& other) {
@@ -31,4 +41,17 @@ int	Fixed::getRawBits() const {
 void	Fixed::setRawBits(int const raw) {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->value = raw;
+}
+
+float	Fixed::toFloat() const { 
+	return ((float)this->value / (1 << fractionalBits));
+}
+
+int	Fixed::toInt() const {
+	return (this->value >> fractionalBits);
+}
+
+std::ostream&	operator<<(std::ostream& os, const Fixed& fixed) {
+	os << fixed.toFloat();
+	return os;
 }
