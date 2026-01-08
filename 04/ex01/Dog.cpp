f09c598ddb0cfc32ca255e3b28684462e53e9e63
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-01-08 20:53:26 +0100

    ex01_Dog

--- 04/ex01/Dog.cpp
+++ 04/ex01/Dog.cpp
@@ -5,25 +5,33 @@
 
 Dog::Dog() : Animal() {
 	type = "Dog";
+	brain = new Brain();
 	std::cout << "Dog default constructor called" << std::endl;	
 }
 
 Dog::Dog(std::string &type) : Animal(type) {
+	brain = new Brain();
 	std::cout << "Dog parameterized constructor called" << std::endl;	
 }
 
 Dog::Dog(const Dog &other) : Animal(other) {
+	this->brain = new Brain(*(other.brain));
 	std::cout << "Dog copy constructor called" << std::endl;	
 }
 
 Dog &Dog::operator=(const Dog &other) { 
-	if (this != &other)
+	if (this != &other) {
 		Animal::operator=(other);
+		if (this->brain)
+			delete this->brain;
+		this->brain = new Brain(*(other.brain));
+	}
 	std::cout << "Dog assigned" << std::endl;
 	return *this;
 }
 
 Dog::~Dog() {
+	delete brain;
 	std::cout << "Dog destructor called" << std::endl;	
 }
 
