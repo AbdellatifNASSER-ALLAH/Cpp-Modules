9d7795c3c096eaf89188db39993c143198eab265
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-05-20 15:16:47 +0100

    ex00_001

--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -7,18 +7,17 @@
 
 class ScalarConverter {
 	private:
-		// TODO: Move constructors and destructor here to make class non-instantiable
-		void	_printChar(const char c);
-		void	_printInt(const int i);
-		//void	_printFloat(const float f);
-		//void	_printDouble(const double d);
-	public:
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter& other);
 		ScalarConverter& operator=(const ScalarConverter& other);
 		~ScalarConverter();
 
-		// TODO: Ensure all methods are static as per subject requirements
+		void	_printChar(const char c);
+		void	_printInt(const int i);
+		//void	_printFloat(const float f);
+		//void	_printDouble(const double d);
+	public:
+
 		static	void	convert(const std::string& str);
 };
 
