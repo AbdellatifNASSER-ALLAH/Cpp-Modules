39b40e0fe5f0f782377a093b2a09585e6379b91c
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-07-08 16:01:44 +0100

    module07_ex02_check_hpp

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -2,6 +2,10 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+#include <iostream>
+
+
 template <typename T>
 class	Array {
 	private:
@@ -21,8 +25,59 @@ class	Array {
 template <typename T>
 Array<T>::Array() : n(0), content( new T()) {}
 
+template <typename T>
+Array<T>::Array(unsigned int n) { 
+	try {
+		content = new T[n]();
+	}catch (std::exception &e) {
+		content = NULL;
+		std::cout << "Exception " << e.what() << std::endl;
+	}
+}
 
 template <typename T>
-Array<T>::~Array() { delete[] content; }
+Array<T>::Array(const Array& other) : n(other.n) {
+	try {
+		content = new T[n]();
+		for (unsigned int i = 0; i < n; i++)
+			content[i] = other.content[i];
+	}catch (std::exception &e) {
+		content = NULL;
+		std::cout << "Exception " << e.what() << std::endl;
+	}
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array& other) {
+	if (this != &other) {
+		if (content)
+			delete[] content;
+		n = other.n;
+		try {
+			content = new T[n]();
+			for (unsigned int i = 0; i < n; i++)
+				content[i] = other.content[i];
+		}catch (std::exception &e) {
+			content = NULL;
+			std::cout << "Exception " << e.what() << std::endl;
+		}
+	}
+	return *this;
+}
+
+template <typename T>
+T& Array<T>::operator[](int i) {
+	if (i < 0 || static_cast<unsigned int>(i) >= n) {
+		throw std::out_of_range("Index out of range");
+	} else {
+		return content[i];
+	}
+}
+
+template <typename T>
+Array<T>::~Array() { 
+	if (content)
+		delete[] content; 
+}
 
 #endif // ARRAY_HPP
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -1,6 +1,10 @@
 #include "Array.hpp"
 
 int main() {
+
 	Array<int> f;
+	Array<int> a(-1);
+
+	std::cout << "End of the program \n";
 	return 0;
 }
