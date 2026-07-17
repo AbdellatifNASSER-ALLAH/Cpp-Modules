0aacd381f6ab6e3c56b8154a1c5689f55af26306
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-07-17 19:44:01 +0100

    module08_ex02

---
+++ 08/ex02/main.cpp
@@ -0,0 +1,96 @@
+#include <exception>
+#include <iostream>
+#include <iterator>
+#include <ostream>
+#include <stack>
+#include <vector>
+#include <string>
+#include <deque>
+
+
+template <typename T, typename Container = std::deque<T> >
+class	MutantStack : public std::stack<T, Container> {
+	public:
+		MutantStack(Container a = Container()) : std::stack<T, Container>(a) {};
+
+		typedef typename Container::iterator iterator;
+
+		iterator begin() {
+			return this->c.begin();
+		};
+		iterator end() {
+			return this->c.end();
+		};
+
+};
+
+void test_basic();
+void test_iteration();
+
+int	main() {
+	void test_basic();
+	void test_iteration();
+	return 0;
+}
+
+void test_basic()
+{
+	std::stack<int> st;
+	MutantStack<int> mst;
+
+	// push
+	for (int i = 1; i <= 5; ++i)
+	{
+		st.push(i);
+		mst.push(i);
+	}
+
+	// top
+	std::cout << "Top std: " << st.top() << std::endl;
+	std::cout << "Top mst: " << mst.top() << std::endl;
+
+	// size
+	std::cout << "Size std: " << st.size() << std::endl;
+	std::cout << "Size mst: " << mst.size() << std::endl;
+
+	// pop
+	st.pop();
+	mst.pop();
+
+	std::cout << "After pop:\n";
+	std::cout << "Top std: " << st.top() << std::endl;
+	std::cout << "Top mst: " << mst.top() << std::endl;
+
+	// empty
+	std::cout << "Empty std: " << st.empty() << std::endl;
+	std::cout << "Empty mst: " << mst.empty() << std::endl;
+
+	// clear all
+	while (!st.empty())
+		st.pop();
+	while (!mst.empty())
+		mst.pop();
+
+	std::cout << "After clearing:\n";
+	std::cout << "Empty std: " << st.empty() << std::endl;
+	std::cout << "Empty mst: " << mst.empty() << std::endl;
+}
+
+void test_iteration()
+{
+	MutantStack<int> mst;
+
+	for (int i = 1; i <= 5; ++i)
+		mst.push(i);
+
+	std::cout << "Iterating mst: ";
+	MutantStack<int>::iterator it = mst.begin();
+	MutantStack<int>::iterator ite = mst.end();
+
+	while (it != ite)
+	{
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << std::endl;
+} 
