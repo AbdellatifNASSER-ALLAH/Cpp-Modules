368f8147fbb2695eb358b01da085b5b4997a2740
Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>
2026-08-13 12:04:13 +0100

    new Signed-off-by: Abdellatif NASSER-ALLAH <nasserallahabdellatif2@gmail.com>

--- 09/ex02/PmergeMe.hpp
+++ 09/ex02/PmergeMe.hpp
@@ -5,11 +5,29 @@
 #include <cstdlib>
 #include <vector>
 #include <deque>
-	
 
 class	PmergeMe {
 	public:
+		typedef int                     value_type;
+		typedef std::deque<value_type>  Deque;
+		typedef std::vector<value_type> Vector;
+
+		static int _nb_cmp;
+
+		static void sort(Vector &a);
+		static void sort(Deque &a);
+		static void prepare(Vector &vec, int ac, char **av);
+
 	private:
+		PmergeMe();
+		PmergeMe(const PmergeMe &other);
+		PmergeMe &operator=(const PmergeMe &other);
+		~PmergeMe();
+
+		static	void	mergeInsert(Vector &vec, int size_g);
+		static	void	pairwiseSort(Vector &vec, int size_g);
+		static	void	insertion(Vector &vec, int size_g);
+
 };
 
 
---
+++ 09/ex02/main.cpp
@@ -0,0 +1,11 @@
+#include "PmergeMe.hpp"
+
+
+int	main(int ac, char **av){
+
+	PmergeMe::Vector	a;
+
+	PmergeMe::prepare(a, ac, av);
+
+	return 0;
+}
